4c95e5214c47826fb7ffc332a984cc50d46c517c
Dan Vyenielo <dvyenielo@gmail.com>
2020-04-11 15:37:05 -0700

    Lab2 Exercise1

--- lab2_intro_to_avr_toolchain/header/simAVRHeader.h
+++ lab2_intro_to_avr_toolchain/header/simAVRHeader.h
@@ -11,8 +11,8 @@ AVR_MCU_VCD_FILE("build/results/lab2_intro_to_avr_toolchain_trace.vcd",1000);
 
 const struct avr_mmcu_vcd_trace_t _mytrace[] _MMCU_ = {
     { AVR_MCU_VCD_SYMBOL("PINA0"), .mask = 1 << 0,.what = (void*)&PINA, } , // Example individual pin
+    { AVR_MCU_VCD_SYMBOL("PORTA"), .what = (void*)&PINA, } , // Example full port
     { AVR_MCU_VCD_SYMBOL("PORTB"), .what = (void*)&PORTB, } , // Example full port
-    { AVR_MCU_VCD_SYMBOL("PINA"), .what = (void*)&PINA, } , // Example individual pin
 };
 
 /* Function to output through UART */
--- lab2_intro_to_avr_toolchain/source/main.c
+++ lab2_intro_to_avr_toolchain/source/main.c
@@ -1,8 +1,9 @@
-/*	Author: lab
- *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
+/*	Author: Daniel Vyenielo
+ *      Partner(s) Name: 
+ *	Lab Section: 21
+ *	Assignment: Lab 2  Exercise 1
+ *	Exercise Description:
+ *	Illuminate PB0 if the garage door is open at night.
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
@@ -12,22 +13,67 @@
 #include "simAVRHeader.h"
 #endif
 
+unsigned char get_bit(unsigned char const i, 
+                      unsigned char const src)
+// returns the i-th bit value found in src.
+{
+    unsigned char ret = src;
+    ret >>= i;
+    ret &= 0x01;
+    return ret;
+}
+
+unsigned char set_bit(unsigned char const i, 
+                      unsigned char const src)
+// sets the i-th bit in src.
+{
+    unsigned char ret = src;
+    ret |= (0x01 << i);
+    return ret;
+}
+
+unsigned char clear_bit(unsigned char const i,
+                        unsigned char const src)
+// clears the i-th bit in src.
+{
+    unsigned char ret = src;
+    ret &= ~(0x01 << i);
+    return ret;
+}
+
+unsigned char toggle_bit(unsigned char const i,
+                         unsigned char const src)
+// toggles the i-th bit in src.
+{
+    unsigned char ret = src;
+    ret ^= (0x01 << i);
+    return ret;
+}
+
 int main(void) {
     /* Insert DDR and PORT initializations */
-    DDRA = 0x00;
-    PORTA = 0xFF;
-    DDRB = 0xFF;
-    PORTB = 0x00;
+    DDRA = 0x00;    // configure PORTA's 8 pins as input
+    PORTA = 0xFF;    // initialize PORTA
+    DDRB = 0xFF;    // configure PORTB's 8 pins as output
+    PORTB = 0x00;    // initialize PORTB
     /* Insert your solution below */
-    unsigned char tmpA = 0x00;
-    unsigned char tmpB = 0x00;
-    while (1) {
-    	tmpA = PINA & 0x01;
-	if(tmpA == 0x01)
-		tmpB = (tmpB & 0xFC) | 0x01;
-	else
-		tmpB = (tmpB & 0xFC) | 0x02;
-	PORTB = tmpB;
+    unsigned char PA0;
+    unsigned char PA1;
+    unsigned char PB0;
+    unsigned char PORTB_TMP = 0x00;
+    while (1) 
+    {
+        PA0 = get_bit(0, PORTA);
+        PA1 = get_bit(1, PORTA);
+        if(PA0 && !PA1)
+        {
+            PORTB_TMP = set_bit(0, PORTB_TMP);
+        }
+	    else
+        {
+	        PORTB_TMP = clear_bit(0, PORTB_TMP);
+        }
+        PORTB = PORTB_TMP;
     }
     return 1;
 }
