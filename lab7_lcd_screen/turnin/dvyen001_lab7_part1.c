7d1dcf87c7c5100cc5c1d9b4e532d45ba76e7ac1
Daniel Vyenielo <dvyenielo@gmail.com>
2020-05-14 19:46:52 -0700

    store lab 8 and 9 notes pre beginning on labs

--- lab7_lcd_screen/turnin/dvyen001_lab7_part1.c
+++ lab7_lcd_screen/turnin/dvyen001_lab7_part1.c
@@ -3,7 +3,7 @@
  *	Lab Section: 21
  *	Assignment: Lab 7  Exercise 1
  *	Exercise Description:
- *	Demo : https://drive.google.com/file/d/13iT8MnMpBZRbNtXrm4287qTV1njehFa5/view?usp=sharing
+ *	Demo : https://drive.google.com/file/d/13xldNsNIf44QnlN1hXj8SgBD2knuw6QR/view?usp=sharing
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
