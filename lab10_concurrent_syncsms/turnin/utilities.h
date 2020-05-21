a08416007d36126845e81876efafb97d21b2cf5b
Daniel Vyenielo <dvyenielo@gmail.com>
2020-05-20 22:40:19 -0700

    lab 10 as submitted.

---
+++ lab10_concurrent_syncsms/turnin/utilities.h
@@ -0,0 +1,12 @@
+unsigned char min(unsigned char const lhs, 
+                  unsigned char const rhs)
+{
+    return lhs < rhs ? lhs : rhs;
+}
+
+unsigned char max(unsigned char const lhs,
+                  unsigned char const rhs)
+{
+    return lhs > rhs ? lhs : rhs;
+}
+
